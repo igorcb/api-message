7ede8f06474ac7ac3d8e1266ea8eadd6bd9ae7e3
Igor C. Batista <igor.batista@gmail.com>
2020-10-22 14:29:23 -0300

    contact association with message

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,8 @@ class Contact < ApplicationRecord
   validates :name, :phone, :email, presence: true
   validates :email, uniqueness: {case_sensitive: false}
   
+  has_many :messages, class_name: "Message", foreign_key: "contact_id", dependent: :destroy
+
   has_paper_trail 
 
   
